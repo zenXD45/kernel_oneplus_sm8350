6d1e6380ba0a8370c3fa0605a1cc6ec05659e3dd
zenXD45 <mparag838@gmail.com>
2026-07-16 14:22:35 +0530

    KernelSU: Integrate ReSukiSU v4.1.0+ (aa327366) with manual hooks
    - Add ReSukiSU as git submodule (commit aa327366, version code 35016)
    - Add KernelSU driver symlink: drivers/kernelsu -> ../KernelSU/kernel
    - Update drivers/Kconfig and drivers/Makefile to include KernelSU
    
    Manual hook patches (CONFIG_KSU_MANUAL_HOOK):
    - fs/stat.c: hook newfstatat, fstatat64, newfstat, fstat64
    - fs/exec.c: hook do_execve, compat_do_execve
    - fs/open.c: hook faccessat
    - kernel/reboot.c: hook sys_reboot
    
    SELinux static symbol exports (CONFIG_KALLSYMS_ALL alternative):
    - security/selinux/selinuxfs.c: unexport sel_handle_status_ops, write_op
    - security/selinux/ss/services.c: unexport context_struct_compute_av,
      security_dump_masked_av
    
    Defconfig (oplus_yupik_QGKI.config):
    - CONFIG_KSU=y
    - CONFIG_KSU_MANUAL_HOOK=y
    - CONFIG_KSU_MULTI_MANAGER_SUPPORT=y
    - CONFIG_KSU_MANUAL_HOOK_AUTO_{SETUID,INITRC,INPUT}_HOOK=y

--- fs/exec.c
+++ fs/exec.c
@@ -1912,6 +1912,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
+#endif
+
 int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
@@ -1926,6 +1932,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1953,6 +1962,9 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit ksud and 32-on-64 support
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -91,7 +91,7 @@ static int context_struct_to_string(struct policydb *policydb,
 				    char **scontext,
 				    u32 *scontext_len);
 
-static void context_struct_compute_av(struct policydb *policydb,
+void context_struct_compute_av(struct policydb *policydb,
 				      struct context *scontext,
 				      struct context *tcontext,
 				      u16 tclass,
@@ -450,7 +450,7 @@ static int dump_masked_av_helper(void *k, void *d, void *args)
 	return 0;
 }
 
-static void security_dump_masked_av(struct policydb *policydb,
+void security_dump_masked_av(struct policydb *policydb,
 				    struct context *scontext,
 				    struct context *tcontext,
 				    u16 tclass,
@@ -610,7 +610,7 @@ void services_compute_xperms_drivers(
  * Compute access vectors and extended permissions based on a context
  * structure pair for the permissions in a particular class.
  */
-static void context_struct_compute_av(struct policydb *policydb,
+void context_struct_compute_av(struct policydb *policydb,
 				      struct context *scontext,
 				      struct context *tcontext,
 				      u16 tclass,
